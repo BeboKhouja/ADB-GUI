2aa38f0816834bea9e6ad3888af56d542956cfad
BeboKhouja <mokkachocolata@gmail.com>
2022-06-22 11:52:28 +0700

    Removed unused JPanel and updated jar

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
-import javax.swing.JPanel;
 
 public class App {
-    static JPanel panel;
     public static void main(String[] args) throws IOException {
         System.out.println("Starting ADB GUI");
         MainFrame myFrame = new MainFrame();
